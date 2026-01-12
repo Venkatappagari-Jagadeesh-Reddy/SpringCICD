b952087307fec813137c1202717506541b5eaf59
codewithreddy-5185 <codewithreddy@gmail.com>
2026-01-12 19:49:03 +0530

    Spring Boot Greeting API

---
+++ src/main/java/com/tap/SpringBootAwscicd1Application.java
@@ -0,0 +1,13 @@
+package com.tap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAwscicd1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAwscicd1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tap/SpringBootAwscicd1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAwscicd1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
